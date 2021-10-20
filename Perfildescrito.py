6789a871903660eb97e6c91c8cf8fe586e594dfa
RyanOliver05 <ryan.silva@arapiraca.ufal.br>
2021-10-19 23:44:43 -0300

    Salvando alterações 2

--- Perfildescrito.py
+++ Perfildescrito.py
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-nome = str(input('NOME: ')).capitalize()
-idade = int(input('IDADE:'))
-genero = str(input('GÊNERO:'))
-
-print(f'My name is {nome},i have {idade} years,and My genre is {genero}')
-print('Saiba que você é especial!')
-=======
 nome = str(input('NOME: ')).capitalize()
 idade = int(input('IDADE:'))
 genero = str(input('GÊNERO:'))
 
 print(f'My name is {nome},i have {idade} years,and My genre is {genero}')
 print("it's not a bug, it's a feature")
->>>>>>> release/1.0.0
